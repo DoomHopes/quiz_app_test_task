695aeb752bce0f3e4ba0ff2172220f6527f2eb90
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-05 15:26:40 +0300

    add new pages

---
+++ lib/pages/one_answer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OneAnswerQuizPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('One Answer Quiz'),
+      ),
+      body: Center(
+        child: Text('Here can be your advertising'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/truefalse_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TrueFalseQuizPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('True or False Quiz'),
+      ),
+      body: Center(
+        child: Text('Here can be your advertising'),
+      ),
+    );
+  }
+}
