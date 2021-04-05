1160522aa3a33dd22bead726ea542caf4d3c357f
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-05 16:02:49 +0300

    add getData methods

--- lib/providers/one_answer_provider.dart
+++ lib/providers/one_answer_provider.dart
@@ -1,3 +1,30 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 
-class OneAnswerProvider extends ChangeNotifier {}
+import 'package:http/http.dart' as http;
+import 'package:quiz_app_test_task/models/oneanswer_model.dart';
+
+class OneAnswerProvider extends ChangeNotifier {
+  List<OneAnswerQuizModel> workList = [];
+  String url = 'https://demo0586388.mockable.io/oneanswer';
+
+  Future<List<OneAnswerQuizModel>> getData() async {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final dynamic jsonData = json.decode(response.body);
+      final List<dynamic> listMap = jsonData;
+      return addToList(listMap);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  List<OneAnswerQuizModel> addToList(List<dynamic> addList) {
+    List<OneAnswerQuizModel> newList = [];
+    newList = addList
+        .map<OneAnswerQuizModel>((json) => OneAnswerQuizModel.fromJson(json))
+        .toList();
+    return newList;
+  }
+}
--- lib/providers/truefalse_provider.dart
+++ lib/providers/truefalse_provider.dart
@@ -1,3 +1,30 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
+import 'package:quiz_app_test_task/models/truefalse_model.dart';
 
-class TrueFalseProvider extends ChangeNotifier {}
+import 'package:http/http.dart' as http;
+
+class TrueFalseProvider extends ChangeNotifier {
+  List<TrueFalseQuizModel> workList = [];
+  String url = 'https://demo0586388.mockable.io/truefalse';
+
+  Future<List<TrueFalseQuizModel>> getData() async {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final dynamic jsonData = json.decode(response.body);
+      final List<dynamic> listMap = jsonData;
+      return addToList(listMap);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  List<TrueFalseQuizModel> addToList(List<dynamic> addList) {
+    List<TrueFalseQuizModel> newList = [];
+    newList = addList
+        .map<TrueFalseQuizModel>((json) => TrueFalseQuizModel.fromJson(json))
+        .toList();
+    return newList;
+  }
+}
